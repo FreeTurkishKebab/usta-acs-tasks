e4bd4adcab538d3d762a5956430120a7007f2d98
FreeTurkishKebab <ardausta.fg@gmail.com>
2022-10-31 10:38:29 +0000

    Tasks

--- 1.reverse.py
+++
@@ -1,3 +0,0 @@
-text = input("Please enter a string")
-reverse = text[:-1]
-print(reverse)
---
+++ tasks/1.reverse.py
@@ -0,0 +1,6 @@
+#require an input
+text = input("Please enter a string")
+#the function allows the reverse of the text to be stored 
+reverse = text[::-1]
+#the reverse is output
+print(reverse)
