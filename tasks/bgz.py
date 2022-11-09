d5ee4b53039a9004dcf71972ef248d2d85e7bbdb
FreeTurkishKebab <ardausta.fg@gmail.com>
2022-11-09 08:56:29 +0000

    Coding the sun

--- tasks/bgz.py
+++ tasks/bgz.py
@@ -1,29 +1,11 @@
-list1 = [1,5,7,18,20,22]
-list2= [6,13,21,24]
-mergeList = []
-length = len(list1) + len(list2)
-length = length - 1
-
-a = 0
-while a <= length : 
-    mergeList.append(a)
-    a = a + 1
-
-i = 0 
-g = 0 
-k = 0
-while i <= length :
-    if list1[g] <= list2[k] :
-        mergeList[i] = list1[g]
-        g = g + 1
-        i = i + 1
-    else :
-        mergeList[i] = list2[k]
-        k = k + 1
-        i = i + 1
-
-z = 0 
-while z <= length :
-    print(mergeList[z])
-    z = z + 1
-
+myString = input("Please input a string to test")
+Answer = False 
+letters = [myString]
+numChars = list(letters)
+numChars = letters[0 : len(myString)]
+reverse = []
+x = 0 
+while x < (len(myString)-1) :
+    reverse[x] = reverse[(len(myString)-1)]
+    x = x + 1
+print(numChars)
