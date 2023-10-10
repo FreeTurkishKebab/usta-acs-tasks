bf083df1c2ca42e06dc6c763510968c03ea3e2b0
FreeTurkishKebab <ardausta.fg@gmail.com>
2023-10-10 18:57:42 +0100

    Update

---
+++ PygameProject/IdleTest.py
@@ -0,0 +1,108 @@
+import pygame 
+import sys 
+from pygame.locals import *
+from pygame import mixer
+#make a background music 
+mixer.init()
+mixer.music.load('MenuM.mp3')
+mixer.music.play()
+
+pygame.init()
+
+
+#define and give values to the dimensions 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+BUTTON_WIDTH = 200
+BUTTON_HEIGHT = 50
+BUTTON_TEXT_COLOR = (0, 0, 0)
+FONT = pygame.font.Font('freesansbold.ttf', 36)
+
+
+#make the a variable to display the screen using screen mode 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Game Menu")
+
+#save an image to add to the background 
+background_image = pygame.image.load("BG.jpg")
+#save an image to add to the buttons 
+wood_texture = pygame.image.load("wood_texture.png")
+
+
+
+#make the classes for the functions and the looks of the button
+class Button:
+    def __init__(self, x, y, width, height, text, action):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.text = text 
+        self.action = action 
+    
+    def draw(self, screen):
+        screen.blit(wood_texture, self.rect)
+        pygame.draw.rect(screen, (0, 0, 0), self.rect, 2) #wooden border 
+        text_surface = FONT.render(self.text, True, BUTTON_TEXT_COLOR)
+        text_rect = text_surface.get_rect(center = self.rect.center)
+        screen.blit(text_surface, text_rect)
+    
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if self.rect.collidepoint(event.pos):
+                self.action()
+def start_game():
+    import ProjectV2
+    open(file="ProjectV2.py")
+
+def quit_game():
+    pygame.quit()
+    sys.exit()
+
+def leaderboard():
+    import LB
+    open(file="LB.py")
+
+#create the buttons on the screen
+start_button = Button(
+    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, 
+    SCREEN_HEIGHT // 2 - BUTTON_HEIGHT // 2 - 50 ,
+    BUTTON_WIDTH,
+    BUTTON_HEIGHT, 
+    "Start",
+    start_game,
+)
+
+quit_button = Button(
+    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, 
+    SCREEN_HEIGHT // 2 - BUTTON_HEIGHT // 2 + 150, 
+    BUTTON_WIDTH,
+    BUTTON_HEIGHT, 
+    "Quit" ,
+    quit_game
+)
+
+LB_button = Button(
+    SCREEN_WIDTH // 2 - BUTTON_WIDTH // 2, 
+    SCREEN_HEIGHT // 2 - BUTTON_HEIGHT // 2 + 50, 
+    BUTTON_WIDTH,
+    BUTTON_HEIGHT, 
+    "Scores",
+    leaderboard
+)
+
+buttons = [start_button, quit_button, LB_button]
+
+#the main game loop to make the menu 
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        for button in buttons:
+            button.handle_event(event)
+
+    screen.blit(background_image, (0, 0)) #draw the background image
+
+    for button in buttons:
+        button.draw(screen)
+
+    pygame.display.flip()
---
+++ PygameProject/LB.py
@@ -0,0 +1,66 @@
+import sys 
+import pygame 
+pygame.init()
+
+font = pygame.font.Font(None, 36)
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+BUTTON_HEIGHT = 82
+BUTTON_WIDTH = 200
+FPS = 60
+
+#create the background
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Leader Board")
+
+CLOCK = pygame.time.Clock()
+
+LB = pygame.image.load("LB2.jpg")
+
+image = pygame.image.load('ARROW.png').convert_alpha()
+    # Set the position of the sprite 
+MENU_BUTTON = image.get_rect() 
+MENU_BUTTON.center=(BUTTON_WIDTH//2, BUTTON_HEIGHT//2)
+BUTTON_TEXT= font.render("Back to Menu", True, (255,255,255))
+
+#create a dictionary
+MY_LEADER_BOARD = [
+    {"Position": "1st place", "Players Name": "GC", "Players Score": 250},
+    {"Position": "2nd place", "Players Name": "TP", "Players Score": 249},
+    {"Position": "3rd place", "Players Name": "GC", "Players Score": 199},
+    ]
+
+def scoreboard():
+
+    y_offset = 200
+    for i in MY_LEADER_BOARD:
+        TEXT = f"{i['Position']}: {i['Players Name']}: {i['Players Score']}"
+        BGTEXT=font.render(TEXT, True, (255,255,255))
+        SCREEN.blit(BGTEXT, (200, y_offset))
+        y_offset += 50
+
+
+
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button==1:
+                if MENU_BUTTON.collidepoint(event.pos):
+                    import Menu
+                    open(file="Menu.py")
+    
+    SCREEN.blit(LB, (0,0))
+    SCREEN.blit(image, (0,0))
+    SCREEN.blit(BUTTON_TEXT, (MENU_BUTTON.x + 30, MENU_BUTTON.y + 30))
+    scoreboard()
+
+    pygame.display.flip()
+    CLOCK.tick(FPS)
+pygame.quit()
+sys.exit()
+
